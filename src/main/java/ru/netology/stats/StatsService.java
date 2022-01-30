67bd6a3c3079d640346d5ac84017f2bb04b0e899
Vitaliy Bozhko <raptor1111@list.ru>
2022-01-30 13:58:31 +0700

    Initial commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,81 @@
+package ru.netology.stats;
+
+public class StatsService {
+
+    public int calcSalesSum(int[] sales) { // Вычисляем сумму всех продаж
+        int sum = 0;
+        for (int sale : sales) {
+            sum += sale;
+        }
+        return sum;
+    }
+
+    public int findSalesAvg(int[] sales) { // Вычисляем среднюю сумму всех продаж
+
+        return calcSalesSum(sales) / sales.length;
+    }
+
+    public int findMaxSales(int[] sales) { // Вычисляем максимальную продажу
+        int currentMax = sales[0];
+        for (int sale : sales) {
+            if (sale > currentMax) {
+                currentMax = sale;
+            }
+        }
+        return currentMax;
+    }
+
+    public int findMinSales(int[] sales) { // Вычисляем минимальную продажу
+        int currentMin = sales[0];
+        for (int sale : sales) {
+            if (sale < currentMin) {
+                currentMin = sale;
+            }
+        }
+        return currentMin;
+    }
+
+    public int calcMaxSalesMonth(int[] sales) { // Находим номер месяца с максимальной продажей
+        int currentMaxMonth = 0;
+        int month = 0;
+        for (int sale : sales) {
+            if (sale >= sales[currentMaxMonth]) {
+                currentMaxMonth = month;
+            }
+            month = month + 1;
+        }
+        return currentMaxMonth + 1;
+    }
+
+    public int calcMinSalesMonth(int[] sales) { // Находим номер месяца с минимальной продажей
+        int currentMinMonth = 0;
+        int month = 0;
+        for (int sale : sales) {
+            if (sale <= sales[currentMinMonth]) {
+                currentMinMonth = month;
+            }
+            month = month + 1;
+        }
+        return currentMinMonth + 1;
+    }
+
+    public int numMonthsMaxSales(int[] sales) { // Считаем количество месяцев с продажами больше средней сумму всех продаж
+        int monthCount = 0;
+        for (int sale : sales) {
+            if (sale > findSalesAvg(sales)) {
+                monthCount = monthCount + 1;
+            }
+        }
+        return monthCount;
+    }
+
+    public int numMonthsMinSales(int[] sales) { // Считаем количество месяцев с продажами меньше средней сумму всех продаж
+        int monthCount = 0;
+        for (int sale : sales) {
+            if (sale < findSalesAvg(sales)) {
+                monthCount = monthCount + 1;
+            }
+        }
+        return monthCount;
+    }
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,80 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+
+    @Test
+    void shouldCalcSalesSum() {
+        StatsService service = new StatsService();
+        int[] managerSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+        int actual = service.calcSalesSum(managerSales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindSalesAvg() {
+        StatsService service = new StatsService();
+        int[] managerSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+        int actual = service.findSalesAvg(managerSales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindMaxSales() {
+        StatsService service = new StatsService();
+        int[] managerSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 20;
+        int actual = service.findMaxSales(managerSales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindMinSales() {
+        StatsService service = new StatsService();
+        int[] managerSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 7;
+        int actual = service.findMinSales(managerSales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalcMaxSalesMonth() {
+        StatsService service = new StatsService();
+        int[] managerSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+        int actual = service.calcMaxSalesMonth(managerSales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalcMinSalesMonth() {
+        StatsService service = new StatsService();
+        int[] managerSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+        int actual = service.calcMinSalesMonth(managerSales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalcNumbMaxSalesMonths() {
+        StatsService service = new StatsService();
+        int[] managerSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.numMonthsMaxSales(managerSales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalcNumbMinSalesMonths() {
+        StatsService service = new StatsService();
+        int[] managerSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.numMonthsMinSales(managerSales);
+        assertEquals(expected, actual);
+    }
+}
